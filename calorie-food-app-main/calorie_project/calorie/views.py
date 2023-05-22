9d1108fe3e7cfcdb7ac5afa602d38e7cb6c75c4f
devikasmenon <73295276+devikasmenon@users.noreply.github.com>
2023-05-22 23:44:53 +0530

    Add files via upload

---
+++ calorie-food-app-main/calorie_project/calorie/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,redirect
+from .models import Food, Consume
+
+def index(request):
+    if request.method == 'POST':
+        consumed_food = request.POST['food_consumed']
+        consume = Food.objects.get(name=consumed_food)
+        user = request.user
+        consume = Consume(user=user,food_consumed=consume)
+        consume.save()
+        foods = Food.objects.all()
+    else:
+        foods = Food.objects.all()
+    consumed_food = Consume.objects.filter(user=request.user)
+    return render(request, 'food/index.html', {'foods': foods, 'consumed_food': consumed_food})
+
+def delete_consume(request,id):
+    consumed_food = Consume.objects.get(id=id)
+    if request.method == 'POST':
+        consumed_food.delete()
+        return redirect('/')
+    return render(request, 'food/delete.html',{'consumed_food': consumed_food})
+
+def delete_all(request):
+    all_items = Consume.objects.all()
+    all_items.delete()
+    return redirect('/')
+    
